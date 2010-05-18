63f40cfc54a4c6b17432961aa30e4d087338ef7c
Michael Prokop <mika@grml.org>
2010-05-18 12:49:14 +0200

    Update grml-debootstrap unittest.

--- grml-debootstrap/deploy.sh
+++ grml-debootstrap/deploy.sh
@@ -8,6 +8,28 @@ sfdisk /dev/sda <<EOF
 ;
 EOF
 
+# cat << EOT | setup-storage -X -f -
+#disk_config sda
+#primary /    0- - -
+#primary swap 20 swap rw
+#EOT
 
-yes y | grml2usb --force --fat16 --bootoptions="netscript=$STATUS/DONE" /live/image /dev/sda1
+MIRROR=http://cdn.debian.net/debian
+HOSTNAME=unittest
+SUITE=stable
+TARGET="/dev/sda1"
+PASSWORD="grml"
+
+# TODO: --bootoptions="netscript=$STATUS/DONE"
+
+echo 'y' | grml-debootstrap -m $MIRROR \
+    -r $SUITE -t $TARGET --hostname $HOSTNAME \
+    --password $PASSWORD
+
+# cat << EOT |grml-chroot $TARGET /bin/bash
+
+cat >> $TARGET /etc/rc.local << EOT
+echo $STATUS/DONE
+EOT
+
 shutdown -h now
